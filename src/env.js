a6b5b51d8aa2510d43ce4c7745e60b24028edd74
Josh <jrogan@wikia-inc.com>
2023-08-13 20:18:36 -0400

    Update linting

--- src/env.js
+++ src/env.js
@@ -1,15 +1,15 @@
 const siteName = process.env.SITE_FAMILY;
 
 export const FAMILIES = {
-  NOLE: 'nole',
-  ROGAN: 'rogan',
-  TOKASH: 'tokash',
-  LOCAL: 'local',
+  NOLE: "nole",
+  ROGAN: "rogan",
+  TOKASH: "tokash",
+  LOCAL: "local",
 };
 
 export function getSiteFamily() {
   if (!siteName) {
-    return FAMILIES.LOCAL
+    return FAMILIES.LOCAL;
   }
 
   return siteName;
